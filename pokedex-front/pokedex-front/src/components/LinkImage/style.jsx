87c2f30e69ec57fa4e3fafb2a50c2fee87d8804d
Ueslei Amaral <uespamaral@gmai.com>
2023-08-12 00:45:39 -0300

    add route

--- pokedex-front/pokedex-front/src/components/LinkImage/style.jsx
+++ pokedex-front/pokedex-front/src/components/LinkImage/style.jsx
@@ -5,13 +5,13 @@ export const ContainerImage = styled.figure`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 219px;
+  height: 180px;
+ 
   background-color: ${(props) => props.backgroundColor || "#BF4F74"};
   border-radius: 10px;
   border: none;
   img {
-    width: 100%;
-    height: 100%;
     padding: 20px;
+    height: 120px;
   }
 `;
--- pokedex-front/pokedex-front/src/components/Title/index.jsx
+++ pokedex-front/pokedex-front/src/components/Title/index.jsx
@@ -1,10 +1,13 @@
 import { ContainerTitle } from "./style";
-
 export const Title = (props) => {
   const myProps = { ...props };
   const number = `${myProps.id}`;
   return (
     <ContainerTitle {...props}>
+      <img
+        src=""
+        alt=""
+      />
       <span>#0{number.padStart(4, "0")}</span> {myProps.title}
     </ContainerTitle>
   );
