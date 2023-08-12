a771c6e6f75114de7b43e04f280a8859718b4527
UESLEI PEREIRA AMARAL <ueslei.pamaral@senacsp.edu.br>
2023-08-11 22:16:32 -0300

    add router search

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { allPokemons } from './pokemon/index.js';
 const app = express();
 const PORT = 3030;
 const pokemons = allPokemons;
-
+const search = [];
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -22,12 +22,19 @@ app.get('/pokemons/:name', (req, res, next) => {
   const name = req.params.name;
 
   for (let i = 0; i < name.length; i++) {
+    search.push(name);
 
-    const pokemon = pokemons.filter(pokemon => pokemon.name[i] === name);
-    res.send(pokemon);
+    for (let index = 0; index < search[0].length; index++) {
+      console.log(search[index])
+      const pokemon = pokemons.filter(pokemon => pokemon.name[index] === search[index]);
+      res.send(pokemon);
+    }
+
+    console.log(search);
+    
   }
+  search.shift();
 
-  
 });
 
 
