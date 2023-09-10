895e611a02e91fc0ff31d05010ca9d761c030696
Ueslei Amaral <uespamaral@gmai.com>
2023-09-10 20:44:38 -0300

    modifield dotenv

--- src/dataBase/connection.js
+++ src/dataBase/connection.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 
 export const connection = () => {
-  const supabaseUrl = process.env.URL;
+  const supabaseUrl = process.env.URL_SUPABASE;
   const supabaseKey = process.env.PIVATE_KEY_SUPABASE;
   const supabase = createClient(supabaseUrl, supabaseKey);
 
